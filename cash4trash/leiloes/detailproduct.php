6b178cac8c055ff58a4061e565d22f074f51a432
Gustavin1501 <guvasconcelos10@gmail.com>
2023-10-26 18:05:17 -0300

    fazendo um négocio pica

---
+++ cash4trash/leiloes/detailproduct.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/detailproduct.css">
+    <title>Cash4Trash</title>
+</head>
+<body>
+<?php
+    include_once("../include/navbar.php");
+?>
+
+    <section class="container sproduct my-5 pt-5">
+        <div class="row mt-5">
+            <div class="col-lg-5 col-md-12 col-12">
+            <img class="img-fluid w-100" src="../imagens/produto.svg" alt="Imagem 1">
+
+            <div class="small-img-group"><!--talvez não use-->
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
